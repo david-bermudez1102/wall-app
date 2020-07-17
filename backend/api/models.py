7c41f6801509a124c6690b16881baf1b82789483
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-07-17 19:17:42 -0400

    Added Message model and migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+ author = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+ content = models.TextField()
+ created = models.DateTimeField(auto_now_add=True)
+ updated = models.DateTimeField(auto_now=True)
+
+ def __str__(self):
+  return self.content
+
