a0219f0183a523b9a0466ca715600ad978007145
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2020-07-18 11:21:47 -0400

    Added basic layout and date-fns package

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,47 @@
 import React from "react"
 import "./App.scss"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Login from "./Components/Login"
 import Wall from "./Components/Wall"
 import SignUp from "./Components/Signup"
 import Store from "./Store"
+import { Layout, Menu } from "antd"
+const { Header, Content, Footer } = Layout
 
 function App() {
 	return (
 		<Store>
 			<Router>
-				<Switch>
-					<Route path={"/login"} component={Login} />
-					<Route path={"/wall"} component={Wall} />
-					<Route path={"/signup"} component={SignUp} />
-				</Switch>
+				<Layout style={{ width: "100%", position: "relative" }}>
+					<Header>
+						<Menu theme='dark' mode='horizontal' defaultSelectedKeys={["2"]}>
+							<Menu.Item key='/login'>
+								<Link to={"/login"}>Login</Link>
+							</Menu.Item>
+							<Menu.Item key='signup'>
+								<Link to={"/signup"}>Signup</Link>
+							</Menu.Item>
+							<Menu.Item key='wall'>
+								<Link to={"/wall"}>Wall</Link>
+							</Menu.Item>
+						</Menu>
+					</Header>
+					<Content
+						style={{
+							padding: 16,
+							position: "relative",
+							width: "100%"
+						}}>
+						<Switch>
+							<Route path={"/login"} component={Login} />
+							<Route path={"/wall"} component={Wall} />
+							<Route path={"/signup"} component={SignUp} />
+						</Switch>
+					</Content>
+					<Footer style={{ textAlign: "center" }}>
+						Wall App - Designed by David Bermudez
+					</Footer>
+				</Layout>
 			</Router>
 		</Store>
 	)
--- frontend/src/Components/Signup/index.js
+++ frontend/src/Components/Signup/index.js
@@ -1,8 +1,15 @@
 import React from "react"
 import SignUpForm from "./SignupForm/SignupForm"
+import { Row, Col } from "antd"
 
 const SignUp = () => {
-	return <SignUpForm />
+	return (
+		<Row gutter={[16, 16]} justify={"center"}>
+			<Col span={8}>
+				<SignUpForm />
+			</Col>
+		</Row>
+	)
 }
 
 export default SignUp
